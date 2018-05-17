c33faaff016eedf40de4969e4a5ade0380f25063
MelonYu <poisson.hust.edu.cn>
2018-05-17 21:37:24 +0800

    do 35

---
+++ LeetCode/35.Search Insert Poisition.py
@@ -0,0 +1,46 @@
+'''
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Example 1:
+
+Input: [1,3,5,6], 5
+Output: 2
+
+Example 2:
+
+Input: [1,3,5,6], 2
+Output: 1
+
+'''
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        
+        low = 0
+        high = len(nums) - 1
+        
+        while low <= high:
+            mid = low + (high - low) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+            
+        return low
+
+if __name__ == '__main__':
+    s = Solution()
+    nums = [1,3,5,6]
+    target = 7
+    print(s.searchInsert(nums, target)) 
