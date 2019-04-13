b83bbb09d4d7907f457a46a5928437071d7c2d50
MelonYu <poisson@hust.edu.cn>
2019-04-14 06:25:52 +0800

    finish the PracticeA

--- Java/thoughtworks-pre-training/W2-T1-Java-Basics/src/main/java/cn/school/thoughtworks/section1/PracticeA.java
+++ Java/thoughtworks-pre-training/W2-T1-Java-Basics/src/main/java/cn/school/thoughtworks/section1/PracticeA.java
@@ -7,6 +7,7 @@ public class PracticeA {
     List<String> collectSameElements(List<String> collection1, List<String> collection2) {
         //实现练习要求，并改写该行代码。
         List<String> theSame = new ArrayList<>(collection1);
+        // use the retainAll method, not the for loop
         theSame.retainAll(collection2);
         return theSame;
     }
